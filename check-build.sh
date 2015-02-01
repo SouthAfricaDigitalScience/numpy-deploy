29a93c77418142abfe0dc4c0c8d138a0865e2746
Bruce Becker <brucellino@gmail.com>
2015-02-01 20:19:12 +0100

    First commit of check-build script for numpy

---
+++ check-build.sh
@@ -0,0 +1,35 @@
+module load ci
+module load python
+module load lapack
+echo ""
+cd $WORKSPACE/$NAME-$VERSION
+python setup.py check
+
+echo $?
+if [ $? != 0 ] ; then
+  exit 1
+fi
+
+python setup.py install --prefix=$SOFT_DIR
+
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION."
+setenv       NUMPY_VERSION       $VERSION
+setenv       NUMPY_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path LD_LIBRARY_PATH   $::env(NUMPY_DIR)/lib
+prepend-path PYTHONPATH   $::env(NUMPY_DIR)
+MODULE_FILE
+) > modules/$VERSION
+
+mkdir -p $LIBRARIES_MODULES/$NAME
+cp modules/$VERSION $LIBRARIES_MODULES/$NAME
