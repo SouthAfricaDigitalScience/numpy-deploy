90c503d0a7520de8c379d6d51efb2a31ca0d84cd
Bruce Becker <brucellino@gmail.com>
2016-06-02 09:10:54 +0200

    Generating the site_cfg in the build script, since the site.cfg is not properly interpreted

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ SOURCE_FILE=$NAME-$VERSION.tar.gz
 module add ci
 module add gcc/${GCC_VERSION}
 module add openblas/0.2.15-gcc-${GCC_VERSION}
+module add fftw/3.3.4-gcc-${GCC_VERSION}-mpi-1.8.8
 module add python/${PYTHON_VERSION}-gcc-${GCC_VERSION}
 export VERSION_MAJOR=${PYTHON_VERSION:0:1} # Should be 2.7 or 3.4 or similar
 export VERSION_MINOR=${PYTHON_VERSION:0:3} # Should be 2.7 or 3.4 or similar
@@ -33,6 +34,28 @@ else
 fi
 tar -xz --keep-newer-files -f ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE}
 # we keep a site.cfg in change control.
-cp site.cfg ${WORKSPACE}/${NAME}-${VERSION}
+# cp site.cfg ${WORKSPACE}/${NAME}-${VERSION}
+# This is not working due to python not reading the variables in site.cfg
+# Not sure how to get the variables to be interpreted within the config,
+# so unfortunately, we have to generate site.cfg on the fly.
+(
+cat <<SITECFG
+[DEFAULT]
+library_dirs = ${OPENBLAS_DIR}/lib
+include_dirs = ${OPENBLAS_DIR}/include/
+ATLAS=none
+BLAS=${OPENBLAS_DIR}/lib/libopenblas.so
+LAPACK=${OPENBLAS_DIR}/lib/libopenblas.so
+[openblas]
+libraries = openblas
+runtime_library_dirs = ${OPENBLAS_DIR}/lib/
+
+[fft]
+libraries = fftw3
+library_dirs = ${FFTW_DIR}/lib/
+include_dirs = ${FFTW_DIR}/include/
+
 cd ${WORKSPACE}/${NAME}-${VERSION}
 python${VERSION_MINOR} setup.py build
+SITECFG
+) >site.cfg
