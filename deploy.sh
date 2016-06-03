5fa530b3383c026f3632667714b2249aa2d77a21
Bruce Becker <brucellino@gmail.com>
2016-06-03 16:08:14 +0200

    v11 doesn't support python clean. maybe if we just regenerate site.cfg the build will work ?

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ VERSION_MINOR=${PYTHON_VERSION:0:3} # Should be 2.7 or 3.4 or similar
 echo $LD_LIBRARY_PATH
 echo ""
 cd $WORKSPACE/$NAME-$VERSION
-python${VERSION_MINOR} setup.py clean
+#python${VERSION_MINOR} setup.py clean
 (
 cat <<SITECFG
 [DEFAULT]
